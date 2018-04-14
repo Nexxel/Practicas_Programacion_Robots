5a80ec3e15b0ff70e5a0f6f95ce96919ffbdec22
Nexxel <sergiogonzalezmuriel@gmail.com>
2018-04-14 22:13:53 +0200

    Corrected my blinking_leds, fulfilled the bicicle mode and corrected some code of the Scorbot

--- Arduino/Implementaciones Sergio/blinkLED_frequency/blinkLED_frequency.ino
+++ Arduino/Implementaciones Sergio/blinkLED_frequency/blinkLED_frequency.ino
@@ -5,7 +5,7 @@ int leds[] = {9, 7, 4, A3};
 void setup() {
   // put your setup code here, to run once:
   pinMode(A5, INPUT);
-  for(int i=0; i<sizeof(leds); i++){
+  for (int i = 0; i < sizeof(leds); i++) {
     pinMode(leds[i], OUTPUT);
   }
 }
@@ -13,27 +13,30 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int frequency = analogRead(TRIMMER_PIN);
-  int[] leds_blink = checkFrequency(frequency);
+  int* leds_blink = checkFrequency(frequency);
   blinkLED(leds_blink, frequency);
 }
 
-int[] checkFrequency(int frequency){
-  if(frequency < 256){
-    return [leds[0]];
-  }else if(frequency >= 256 || frequency < 512){
-    return [leds[0], leds[1]];
-  }else if(frequency >= 512 || frequency < 768){
-    return [leds[0], leds[1], leds[2]];
-  }else{
+int* checkFrequency(int frequency) {
+  if (frequency < 256) {
+    int leds_blink[] = {leds[0]};
+    return leds_blink;
+  } else if (frequency >= 256 || frequency < 512) {
+    int leds_blink[] = {leds[0], leds[1]};
+    return leds_blink;
+  } else if (frequency >= 512 || frequency < 768) {
+    int leds_blink[] = {leds[0], leds[1], leds[2]};
+    return leds_blink;
+  } else {
     return leds;
   }
 }
 
-void blinkLED(int[] leds_blink,int frequency){
-  for(int i=0; i<sizeof(leds_blink); i++){
-    digitalWrite(led[i], HIGH);
+void blinkLED(int* leds_blink, int frequency) {
+  for (int i = 0; i < sizeof(leds_blink); i++) {
+    digitalWrite(leds_blink[i], HIGH);
     delay(frequency);
-    digitalWrite(led[i], LOW);
+    digitalWrite(leds_blink[i], LOW);
     delay(frequency);
   }
 }
--- Scorbot/getAndSavePositions.m
+++ Scorbot/getAndSavePositions.m
@@ -2,19 +2,17 @@ function [ positions, gripper ] = getAndSavePositions( scorbot )
 %GETANDSAVEPOSITIONS Get the positions for the Scorbot to follow and save them 
 %   positions: Array of positions for the Scorbot to follow.
     positions = []; gripper = [];
-	fprintf('----> Teach the robot where is the location for picking items and press Enter to finish.\n\n');
-	in = 1;
-    while in
+	fprintf('----> Teach the robot where are each of the four locations you need, pressing Enter on each position to save it.\n\n');
+    for i = 1:4
         suministro=scorbot.pendant();
         positions = [positions suministro];
-        open_or_close = input('Introduce 1 for open the gripper or 0 in other case: ');
+        open_or_close = input('Introduce 1 the gripper must be open or 0 in other case: ');
         gripper = [gripper open_or_close];
-        in = input('Introduce un número distinto de 0 para continuar: ');
     end
     %% Save positions
     ok = input('Introduce 1 to save these positions and 0 in other case: ');
     if ok == 1
-        fichero_output = input('Introduce the name of the file where you want to save the positions (Without the .mat', 'scorbot');
+        fichero_output = input('Introduce the name of the file where you want to save the positions (Without the .mat), ', 's');
         fichero_output = strcat(fichero_output, '.mat');
         save(fichero_output, positions, gripper);
     end
